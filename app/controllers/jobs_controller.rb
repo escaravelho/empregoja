cb0fe4abfdf2babaa5528498c94566e028374a1f
Sandro <foxhound@campuscode.com>
2015-09-10 20:43:27 -0300

    Add new jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,16 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(params.require(:job).permit(:title, :location, :category, :company, :description))
+
+    if @job.save
+      redirect_to @job
+    end
+  end
 end
