f6aa56e7ac13256b8d658f8eb29925131bf2fda5
Sandro <foxhound@campuscode.com>
2015-09-17 21:39:34 -0300

    User edit a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,15 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update]
+  before_action :set_collection, only: [:index, :new, :create, :edit]
   def index
     @jobs = Job.all
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
-    @categories = Category.all
   end
 
   def create
@@ -21,13 +18,29 @@ class JobsController < ApplicationController
       redirect_to @job
     else
       flash[:error] = "Warning! All fields are mandatory."
-      @companies = Company.all
-      @categories = Category.all
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    @job.update(job_params)
+    redirect_to @job
+  end
+
+
   private
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_collection
+    @companies = Company.all
+    @categories = Category.all
+  end
 
   def job_params
     params.require(:job).permit(:title, :location, :category_id, :company_id, :description, :featured)
