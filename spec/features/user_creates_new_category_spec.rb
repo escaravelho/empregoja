e7e5d5b868e90fb2e35978fc9a4c3391d78e1232
Sandro <foxhound@campuscode.com>
2015-09-21 19:51:43 -0300

    User create a new category

--- spec/features/user_creates_new_category_spec.rb
+++ spec/features/user_creates_new_category_spec.rb
@@ -6,7 +6,7 @@ feature 'User creates a new category' do
     category = Category.new(name: 'Desenvolvedor')
 
     visit new_category_path
-
+  
     fill_in 'Name',     with: category.name
     click_on 'Criar Categoria'
 
