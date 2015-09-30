26235a55b95c5ca9a89c93c1aabe62ba9995f401
Sandro <foxhound@campuscode.com>
2015-09-29 21:56:37 -0300

    user update company test

---
+++ spec/features/user_update_company_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'User update a company' do
+  scenario 'successfully' do
+    company = Company.create! name: "New Riders",
+                              phone: "66625535",
+                              mail: "riders@teste.com.br",
+                              location: "New York"
+
+    visit edit_company_path(company)
+
+    fill_in "Phone", with: "111111199"
+
+    click_on 'Atualizar Empresa'
+
+    expect(page).to have_content("111111199")
+  end
+
+  scenario 'invalid field' do
+    company = Company.create! name: "New Riders",
+                              phone: "66625535",
+                              mail: "riders@teste.com.br",
+                              location: "New York"
+
+    visit edit_company_path(company)
+
+    fill_in "Name", with: ""
+
+    click_on 'Atualizar Empresa'
+
+    expect(page).to have_content("Warning! All fields are mandatory")
+
+  end
+end
