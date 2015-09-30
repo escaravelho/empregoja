dbdd1238d89d42f6a58af322327791e2680ac72d
Sandro <foxhound@campuscode.com>
2015-09-29 21:56:50 -0300

    user update job test

---
+++ spec/features/user_update_job_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'User update job' do
+  scenario 'successfully'do
+  company = Company.create! name: "Campus Code",
+                phone: "984458545",
+                mail: "campus@campuscode.com.br",
+                location: "Acre"
+
+  category = Category.create! name: "DBA"
+
+  job = Job.create! title: "Desenvolvedor",
+              location: "São Paulo",
+              company: company,
+              category: category,
+              description: "Vaga de desenvolvimento para Campus Codeiros"
+
+  visit edit_job_path(job)
+
+  fill_in "Title", with: "Homologador"
+
+  click_on 'Atualizar Vaga'
+
+  expect(page).to have_content("Homologador")
+  end
+end
