de4f25eb38b220b097ad0dd71340c596d15a7d58
Sandro <foxhound@campuscode.com>
2015-09-29 21:56:15 -0300

    user create new job test

---
+++ spec/features/user_create_new_job_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'User create a new job' do
+  scenario 'successfully' do
+
+    Company.create! name: "Locaweb",
+                  phone: "12312346",
+                  mail: "locaweb@locaweb.com.br",
+                  location: "Salvador"
+
+    Category.create! name: "Desenvolvimento"
+
+
+    visit new_job_path
+
+    fill_in "Title", with: "Desenvolvedor Rails"
+    fill_in "Location", with: 'Salvador'
+    select "Locaweb", from: 'job[company_id]'
+    select 'Desenvolvimento', from: "job[category_id]"
+    fill_in "Description", with: 'Vaga para dev preguiçoso'
+
+    click_on 'Criar Vaga'
+
+    expect(page).to have_content "Desenvolvedor Rails"
+
+  end
+
+  scenario 'invalid fields' do
+
+   visit new_job_path
+
+   click_on 'Criar Vaga'
+
+   expect(page).to have_content('Warning! All fields are mandatory')
+
+  end
+end
