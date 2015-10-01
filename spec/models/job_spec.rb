f4c47f05660197ec9423753c9456f23db04da725
Sandro <foxhound@campuscode.com>
2015-09-30 22:07:43 -0300

    Add job model tests

---
+++ spec/models/job_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe Job do
+  it 'is recent' do
+
+    job = create(:job)
+
+    expect(job).to be_recent
+  end
+
+  it 'is not recent' do
+
+    job = nil
+
+    travel_to 20.days.ago do
+      job = create(:job)
+    end
+
+    expect(job).to_not be_recent
+  end
+
+  it 'is expired' do
+
+    job = nil
+
+    travel_to 120.days.ago do
+      job = create(:job)
+    end
+
+    expect(job).to be_expired
+  end
+
+  it 'is not expired' do
+
+    job = nil
+
+    travel_to 20.days.ago do
+      job = create(:job)
+    end
+
+    expect(job).to_not be_expired
+  end
+end
